9a8584bc48a8886fce1902d89f25beb03cc1d5ef
Diego Galante <diegogalantte@gmail.com>
2021-06-21 20:07:16 -0300

    Initial Commit

---
+++ AzuereDevopsStateTracker/Entities/WorkItem.cs
@@ -0,0 +1,16 @@
+﻿namespace AzuereDevopsStateTracker.Entities
+{
+    public class WorkItem
+    {
+        public int Id { get; private set; }
+        public string AssignedTo { get; private set; }
+        public string Type { get; private set; }
+
+        public WorkItem(int id, string assignedTo, string type)
+        {
+            Id = id;
+            AssignedTo = assignedTo;
+            Type = type;
+        }
+    }
+}
