88fe16769437d99717bf3617c23a06f8776de51a
Elvis Souza <elvis.souza@gruposize.com.br>
2022-01-22 16:49:21 -0300

    Message integration base class functions

--- AzureDevopsTracker/Integrations/MessageIntegration.cs
+++ AzureDevopsTracker/Integrations/MessageIntegration.cs
@@ -1,21 +1,42 @@
 ﻿using AzureDevopsTracker.Entities;
 using System;
-using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 
 namespace AzureDevopsTracker.Integrations
 {
-    internal abstract class MessageIntegration
+    public abstract class MessageIntegration
     {
-        internal abstract void Send(ChangeLog changeLog);
+        public abstract void Send(ChangeLog changeLog);
+        private static readonly string MIMETYPE = "application/json";
 
-        internal string GetTitle(ChangeLog changeLog)
+        protected string GetTitle(ChangeLog changeLog)
         {
-            System.Resources.ResourceManager mgr = new
-                System.Resources.ResourceManager("AzureDvevopsTracker.Resource",
-                System.Reflection.Assembly.GetExecutingAssembly());
+            return $"Nova atualização da plataforma";
+        }
 
-            return "";
+        protected string GetVersion(ChangeLog changeLog)
+        {
+            return $"Versao: { changeLog.Number}";
+        }
+
+        protected string GetNugetVersion()
+        {
+            System.Reflection.Assembly assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            System.Diagnostics.FileVersionInfo fileVersionInfo = System.Diagnostics.FileVersionInfo.GetVersionInfo(assembly.Location);
+            return $"Powered by Typing Hard • { fileVersionInfo.FileVersion }";
+        }
+
+        protected HttpResponseMessage Notify(object body)
+        {
+            var json = JsonSerializer.Serialize(body);
+            var content = new StringContent(json,
+                                            Encoding.UTF8,
+                                            MIMETYPE);
+            HttpClient client = new HttpClient();
+            return client.PostAsync(new Uri(MessageConfig.URL),
+                content).Result;
         }
     }
 }
