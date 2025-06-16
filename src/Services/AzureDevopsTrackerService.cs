bad4c796896cd5a2ee6edcee8157821ca7226eee
Elvis Souza <elvis.souza@gruposize.com.br>
2025-06-16 10:00:09 -0300

    Fixing WorkItem update when no fields are sent | Including READ.md for Nuget.org

--- src/Services/AzureDevopsTrackerService.cs
+++ src/Services/AzureDevopsTrackerService.cs
@@ -242,8 +242,8 @@ namespace AzureDevopsTracker.Services
 
         public void AddWorkItemChange(WorkItem workItem, UpdatedWorkItemDto update)
         {
-            if (update.Resource.Fields.State is null) return;
-            if (update.Resource.Fields.StateChangeDate is null) return;
+            if (update?.Resource?.Fields?.State is null) return;
+            if (update?.Resource?.Fields?.StateChangeDate is null) return;
 
             var changedBy = update.Resource.Revision.Fields.ChangedBy ?? update.Resource.Fields.ChangedBy.NewValue;
             var workItemChange = ToWorkItemChange(workItem.Id,
